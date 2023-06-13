a3763663e195818a225357c0607409084cc17a33
sigged <sderdeyn@gmail.com>
2023-06-14 01:04:23 +0200

    Force light mode on all platforms

--- Mde.ExploreUI/Platforms/Android/MainActivity.cs
+++ Mde.ExploreUI/Platforms/Android/MainActivity.cs
@@ -1,10 +1,17 @@
 ﻿using Android.App;
 using Android.Content.PM;
 using Android.OS;
+using AndroidX.AppCompat.App;
 
 namespace Mde.ExploreUI;
 
 [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
 public class MainActivity : MauiAppCompatActivity
 {
+    protected override void OnCreate(Bundle savedInstanceState)
+    {
+        AppCompatDelegate.DefaultNightMode = AppCompatDelegate.ModeNightNo;
+
+        base.OnCreate(savedInstanceState);
+    }
 }
