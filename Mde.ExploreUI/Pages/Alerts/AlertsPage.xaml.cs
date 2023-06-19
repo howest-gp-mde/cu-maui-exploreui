233861f7f0d7beee153a6a5e9a5148e33522a4d6
sigged <sderdeyn@gmail.com>
2023-06-19 14:26:09 +0200

    Add images page

--- Mde.ExploreUI/Pages/Alerts/AlertsPage.xaml.cs
+++ Mde.ExploreUI/Pages/Alerts/AlertsPage.xaml.cs
@@ -11,7 +11,6 @@ public partial class AlertsPage : ContentPage
 		InitializeComponent();
 	}
 
-
     private async void btnAlert_Clicked(object sender, EventArgs e)
     {
         string title = "Pop-up!";
