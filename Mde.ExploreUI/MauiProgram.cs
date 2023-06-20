74979e1bc68f94b8061de973a878a66a85dcd9c1
sigged <sderdeyn@gmail.com>
2023-06-20 17:31:08 +0200

    Add iconfonts

--- Mde.ExploreUI/MauiProgram.cs
+++ Mde.ExploreUI/MauiProgram.cs
@@ -16,6 +16,10 @@ public static class MauiProgram
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
 				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
                 fonts.AddFont("Sacramento-Regular.ttf", "Sacramento");
+                fonts.AddFont("LooneyTunes.ttf", "Looney");
+                fonts.AddFont("Font Awesome 6 Brands-Regular-400.otf", "FABrands");
+                fonts.AddFont("Font Awesome 6 Free-Regular-400.otf", "FARegular");
+                fonts.AddFont("Font Awesome 6 Free-Solid-900.otf", "FASolid");
             });
 
 #if DEBUG
