78203cfbd8563a3449b5441b7a0a61f5e1294418
sigged <sderdeyn@gmail.com>
2023-06-23 00:42:31 +0200

    Finalize progress page

--- Mde.ExploreUI/Pages/Forms/PickersPage.xaml.cs
+++ Mde.ExploreUI/Pages/Forms/PickersPage.xaml.cs
@@ -1,3 +1,5 @@
+using System.Text;
+
 namespace Mde.ExploreUI.Pages.Forms;
 
 public partial class PickersPage : ContentPage
@@ -6,4 +8,73 @@ public partial class PickersPage : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    protected override void OnAppearing()
+    {
+        //set date and time pickers
+        datePicker.MinimumDate = DateTime.Today;
+        timePicker.Time = DateTime.Now.TimeOfDay;
+
+        //set course picker items
+        var courses = GetCourses();
+        coursePicker.ItemsSource = courses;
+    }
+
+    private async void btnSubmit_Clicked(object sender, EventArgs e)
+    {
+        var selectedCourse = coursePicker.SelectedItem as Course;
+
+        var summary = new StringBuilder();
+        summary.AppendLine($"Start date: {datePicker.Date:ddd dd MMM yyyy}");
+        summary.AppendLine($"Start time: {timePicker.Time:hh\\:mm}"); 
+        summary.AppendLine();
+
+        if (selectedCourse != null)
+        {
+            summary.AppendLine($"Course: {selectedCourse.Name}\n({selectedCourse.Credits} credits, {selectedCourse.Duration.TotalDays:N0} days)");
+        }
+
+        await DisplayAlert(
+            "Submitted!",
+            summary.ToString(),
+            "OK");
+    }
+
+    private List<Course> GetCourses()
+    {
+        return new List<Course>
+        {
+            new Course
+            {
+                Name = "Programming Basics",
+                Credits = 6,
+                Duration = new TimeSpan(20, 0, 0, 0)
+            },
+            new Course
+            {
+                Name = "Programming Advanced",
+                Credits = 3,
+                Duration = new TimeSpan(10, 0, 0, 0)
+            },
+            new Course
+            {
+                Name = "Programming Expert",
+                Credits = 6,
+                Duration = new TimeSpan(20, 0, 0, 0)
+            },
+            new Course
+            {
+                Name = "Mobile Development",
+                Credits = 9,
+                Duration = new TimeSpan(30, 0, 0, 0)
+            },
+        };
+    }
+}
+
+public class Course
+{
+    public string Name { get; set; }
+    public int Credits { get; set; }
+    public TimeSpan Duration { get; set; }
 }
--- Mde.ExploreUI/Pages/Inform/ProgressPage.xaml.cs
+++ Mde.ExploreUI/Pages/Inform/ProgressPage.xaml.cs
@@ -1,9 +1,63 @@
+using Android.Icu.Number;
+using Microsoft.Maui.Animations;
+using Microsoft.Maui.Layouts;
+
 namespace Mde.ExploreUI.Pages.Inform;
 
 public partial class ProgressPage : ContentPage
 {
-	public ProgressPage()
-	{
-		InitializeComponent();
-	}
+    public ProgressPage()
+    {
+        InitializeComponent(); 
+    }
+
+
+    private int currentColorIndex = 0;
+    private int targetColorIndex = 1;
+
+    private Color[] colors = new Color[]
+    {
+        Colors.Red,
+        Colors.Blue,
+        Colors.Green,
+        Colors.Yellow,
+    };
+
+    protected override async void OnAppearing()
+    {
+        double targetColorProgress = 0;
+
+        //animation loop
+        while(this.IsVisible)
+        {
+            await Task.Delay(25);
+
+            //animate progressbar
+            progressBar.Progress += 0.005;
+            if (progressBar.Progress >= 1)
+            {
+                await Task.Delay(2000);
+                progressBar.Progress = 0;
+            }
+
+            //animate coloredBusyIndicator
+            if (targetColorProgress >= 1)
+            {
+                targetColorProgress = 0;
+                currentColorIndex = targetColorIndex;
+                targetColorIndex++;
+
+                if (targetColorIndex >= colors.Length)
+                {
+                    targetColorIndex = 0;
+                }
+            }
+            else
+            {
+                targetColorProgress = Math.Min(1, targetColorProgress + 0.01);
+            }
+
+            coloredBusyIndicator.Color = colors[currentColorIndex].Lerp(colors[targetColorIndex], targetColorProgress);
+        }
+    }
 }
